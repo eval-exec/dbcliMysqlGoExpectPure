af849c2af60a58023d16aa12f1cd519d444eb65a
slarsar <slarsar@yandex.com>
2019-05-18 10:04:47 +0800

    input and output and err redirect

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os/exec"
 	"strings"
-
-	"database/sql"
 
 	"github.com/gdamore/tcell"
 	_ "github.com/go-sql-driver/mysql"
@@ -15,10 +14,13 @@ import (
 func main() {
 
 	var out string
-
+	var outErr string
 	var getcommands string
 
+
+
 	var datatext string
+	var execCommand *exec.Cmd
 	datatext = "ok I am data text"
 
 	newPrimitive := func(text string) tview.Primitive {
@@ -38,14 +40,18 @@ func main() {
 
 			getcommands = tinput.GetText()
 			commandsSlice := strings.Fields(getcommands)
-			execCommand := exec.Command(commandsSlice[0], commandsSlice[1:]...)
-			output, err := execCommand.CombinedOutput()
-			if err != nil {
-				datatext = fmt.Sprintf("%s", err)
-			}
+			execCommand = exec.Command(commandsSlice[0], commandsSlice[1:]...)
 
-			out = fmt.Sprint(string(output))
-			datatext = fmt.Sprintf("%s", out)
+			outputBtte, err := execCommand.CombinedOutput()
+			if err != nil {
+				outErr = fmt.Sprint(err)
+			} else {
+				outErr = ""
+			}
+			out = string(outputBtte)
+
+
+			datatext = fmt.Sprintf("%s\n%s", out, outErr)
 
 			main.SetText(datatext)
 			tinput.SetText("")
@@ -70,12 +76,27 @@ func main() {
 	}
 }
 
-func DBexec(tDB **sql.DB, str string) (returnRows *sql.Rows, err error) {
-	returnRows, err = (*tDB).Query(str)
-	if err != nil {
-		debug(err, "err in dbexec func ")
+func copyAndCapture(w io.Writer, r io.Reader) ([]byte, error) {
+	var out []byte
+	buf := make([]byte, 1024, 1024)
+	for {
+		n, err := r.Read(buf[:])
+		if n > 0 {
+			d := buf[:n]
+			out = append(out, d...)
+			_, err := w.Write(d)
+			if err != nil {
+				return out, err
+			}
+		}
+		if err != nil {
+			// Read returns io.EOF at the end of file, which is not an error for us
+			if err == io.EOF {
+				err = nil
+			}
+			return out, err
+		}
 	}
-	return
 }
 
 func debug(str interface{}, stxr string) {
@@ -84,7 +105,3 @@ func debug(str interface{}, stxr string) {
 	fmt.Println(stxr)
 	fmt.Println("--------------------------------------------------------")
 }
-
-
-
-
